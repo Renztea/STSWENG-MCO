08c19440463aecb38da2638a9a9dc9172c18f532
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2022-10-31 14:43:40 +0800

    Delete Project directory

--- Project/public/js/helper.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    Handlebars.registerHelper('select', function(value, options) {
-        console.log("testvalue: ", value);
-        var $el = $('<select />').html(options.fn(this));
-        $el.find('[value="' + value + '"]').attr({'selected': 'selected'});
-        return $el.html();
-    });
-});
