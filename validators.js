4c7e7ffe879d17c918d9a420e8d7b482a8b4edff
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2022-11-16 20:47:00 +0800

    Revert "Added input validation for addCake"
    This reverts commit 66b51c793a479ace7b3a87cda436c285848b966f.

--- validators.js
+++ validators.js
@@ -8,13 +8,14 @@ const loginValidation = [
 const addProductValidation = [
   body('productName').not().isEmpty().withMessage("Product name is required!"),
   body('productPricesVanilla1').not().isEmpty().withMessage("Price is required! If a cake does not have a specific size and flavor just put 0!")
-    .isInt({min : 0}).withMessage("Inputted price is not valid"),
+    .isEmpty().isInt({min : 0},{ allow_leading_zeroes: true }).withMessage("Inputted price is not valid"),
   body('productPricesVanilla2').not().isEmpty().withMessage("Price is required! If a cake does not have a specific size and flavor just put 0!")
-    .isInt({min : 0}).withMessage("Inputted price is not valid"),
+    .isEmpty().isInt({min : 0},{ allow_leading_zeroes: true }).withMessage("Inputted price is not valid"),
   body('productPricesChocolate1').not().isEmpty().withMessage("Price is required! If a cake does not have a specific size and flavor just put 0!")
-    .isInt({min : 0}).withMessage("Inputted price is not valid"),
-  body('productPricesChocolate2').not().isEmpty().withMessage("Price is required! If a cake does not have a specific size and flavor just put 0!")
-    .isInt({min : 0}).withMessage("Inputted price is not valid"),
+    .isEmpty().isInt({min : 0},{ allow_leading_zeroes: true }).withMessage("Inputted price is not valid"),
+  body('productPricesChocolate2').not().isEmpty().withMessage("Price is requirved! If a cake does not have a specific size and flavor just put 0!")
+    .isEmpty().isInt({min : 0},{ allow_leading_zeroes: true }).withMessage("Inputted price is not valid"),
+  
   //body('filename').not().isEmpty().withMessage("Product image is required!"),
 ]
 
