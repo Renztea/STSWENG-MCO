ce5734be98d830ea0298fc5a8b1b42a50a4e22b3
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2022-11-19 16:13:17 +0800

    Updated order.ejs

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ejs = require('ejs') // Added
 const { validationResult } = require('express-validator');
 const Cake = require('../models/cake')
 const Cupcake = require('../models/cupcake')
@@ -143,40 +144,37 @@ const controller = {
 
     getOrdersPage: async function(req, res) { //Added Here(John)
         var category = req.params.category
-        var pageNumber = req.params.numberPage
-        var orders;
+        var pageNumber = req.query.pageNumber || 0
+        var orderList;
         var orderCount;
         var offSet = 0;
+
+        console.log("cat: " + category, "\npageNumber: " + pageNumber);
 
         if(pageNumber > 0) {
           offSet = pageNumber - 1;
         }
-
-        orderCount = 2;
-
-        orders = [{orderDate: "11/12/2022", payDate: "", pickedUpDate: "", cancelledDate: "", orderId: "11122022001",
-                    name: "Jeff", cellphoneNo: "0123456789", price: "1000", status: "UNPAID"}, 
-                {orderDate: "11/12/2022", payDate: "", pickedUpDate: "", cancelledDate: "", orderId: "11122022002",
-                name: "Josh", cellphoneNo: "0123456789", price: "2000", status: "PAID"}]
+        
         /*
         if (category == 'All') {
             orders = Orders.find({}).limit(5).skip(5 * offSet)
             orderCount = Orders.find({}).count()
         } else {
             orders = Orders.find({"Status": category}).limit(5).skip(5 * offSet)
-          orderCount = Orders.find({"Status": category}).count()
+            orderCount = Orders.find({"Status": category}).count()
         }
         */
+        
 
+       orderList["category"] = category;
         if(pageNumber > 0) {
-          var options = "cache";
-          var dir = path.join(__dirname, '../views/partials/');
-          
-          ejs.renderFile(dir + 'orderList.ejs', orders, options, function(err, str) {       
-              res.send(str);
-          });
+            var options = "cache: false";
+            var dir = path.join(__dirname, '../views/partials/');
+            ejs.renderFile(dir + 'orderList.ejs', {orderList: orderList}, options, function(err, str) { 
+                res.send(str);
+            });
         } else {
-          res.render('order', {orderList: orders, count: orderCount, category: category})
+            res.render('order', {orderList: orderList, count: orderCount})
         }
     }
 
